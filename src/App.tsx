0a177a33f576763d59a7f7145d43a0001b5ecb6c
viliusling@gmail.com <viliusling@gmail.com>
2021-04-25 12:31:41 +0300

    Comment unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const CryptoJS = require("crypto-js");
+// const CryptoJS = require("crypto-js");
 
-const apiKey = process.env.REACT_APP_BITFINEX_API_KEY;
-const apiSecret = process.env.REACT_APP_BITFINEX_API_KEY_SECRET;
+// const apiKey = process.env.REACT_APP_BITFINEX_API_KEY;
+// const apiSecret = process.env.REACT_APP_BITFINEX_API_KEY_SECRET;
 
-const apiHost = "https://api.bitfinex.com";
-const apiPath = "/v2/auth/r/wallets";
-const nonce = (Date.now() * 1000000000).toString();
-const body = {};
-const signature = `/api/${apiPath}${nonce}${JSON.stringify(body)}`;
-const sig = CryptoJS.HmacSHA384(signature, apiSecret).toString();
+// const apiHost = "https://api.bitfinex.com";
+// const apiPath = "/v2/auth/r/wallets";
+// const nonce = (Date.now() * 1000000000).toString();
+// const body = {};
+// const signature = `/api/${apiPath}${nonce}${JSON.stringify(body)}`;
+// const sig = CryptoJS.HmacSHA384(signature, apiSecret).toString();
 
 function App() {
   React.useEffect(() => {
@@ -31,24 +31,24 @@ function App() {
       });
   }
 
-  function getWallet() {
-    fetch(`${apiHost}${apiPath}`, {
-      method: "POST",
-      body: JSON.stringify(body),
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-        "bfx-nonce": nonce,
-        "bfx-apikey": apiKey!,
-        "bfx-signature": sig!,
-      },
-    })
-      .then((res) => res)
-      .then((json) => console.log("RESPONSAS: ", json))
-      .catch((err) => {
-        console.log("ERRORAS: ", err);
-      });
-  }
+  // function getWallet() {
+  //   fetch(`${apiHost}${apiPath}`, {
+  //     method: "POST",
+  //     body: JSON.stringify(body),
+  //     mode: "no-cors",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "bfx-nonce": nonce,
+  //       "bfx-apikey": apiKey!,
+  //       "bfx-signature": sig!,
+  //     },
+  //   })
+  //     .then((res) => res)
+  //     .then((json) => console.log("RESPONSAS: ", json))
+  //     .catch((err) => {
+  //       console.log("ERRORAS: ", err);
+  //     });
+  // }
 
   return <div>Hello</div>;
 }
